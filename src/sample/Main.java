f4e37dc772dd776f9eb1e0f7b7a12e89aaac28df
JohannesSchreurs <johannes@basedesign.com>
2019-09-01 18:35:32 +0200

    Added startscreen to select a survey

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -10,44 +10,49 @@ import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import model.Answer;
+import model.Question;
+import model.Survey;
 import model.User;
+import Controller.UserController;
+import Controller.SurveyController;
+import Controller.QuestionController;
+import Controller.AnswerController;
 
 public class Main extends Application {
+
+    UserController userController = new UserController();
+    SurveyController surveyController = new SurveyController();
+    QuestionController questionController = new QuestionController();
+    AnswerController answerController = new AnswerController();
+
+    Survey activeSurvey;
+    Question activeQuestion;
 
     @Override
     public void start(Stage primaryStage) throws Exception {
         StackPane root = new StackPane();
-        Button button = new Button("Go To Second Form");
-        Label label = new Label("Enter your username");
-        label.setLayoutX(100);
-        label.setLayoutY(100);
-        root.getChildren().add(button); 
-        root.getChildren().add(label);
-        Scene scene = new Scene(root, 500,500); // create the scene and set the root, width and height
-        primaryStage.setScene(scene); // set the scene
-        primaryStage.setTitle("Register page");
-        primaryStage.show();
+        ListView<Survey> listView = new ListView();
 
-        // add action listener, I will use the lambda style (which is data and code at the same time, read more about it in Oracle documentation)
-        button.setOnAction(e->{
-            //primaryStage.close(); // ndex++;
-            // create the structure again for the second GUI
-            // Note that you CAN use the previous root and scene and just create a new Stage
-            //(of course you need to remove the button first from the root like this, root.getChildren().remove(0); at index 0)
-            StackPane newRoot = new StackPane();
-            Label label2 = new Label("Select a survey");
-            newRoot.getChildren().add(label2);
-            Scene secondScene = new Scene(newRoot, 500,500);
-            Stage secondStage = new Stage();
-            secondStage.setScene(secondScene); // set the scene
-            secondStage.setTitle("select a survey");
-            secondStage.show();
-            primaryStage.close(); // close the first stage (Window)
+        for(Survey u : surveyController.getSurveys()) {
+            System.out.println(u.getName() + ", id:" + u.getId());
+            listView.getItems().add(u);
+        }
+
+        listView.setOnMouseClicked(e -> {
+            System.out.println("Clicked on survey " + listView.getSelectionModel().getSelectedItem());
+            activeSurvey = listView.getSelectionModel().getSelectedItem();
+            System.out.println(activeSurvey.getName() + " " + activeSurvey.getId());
         });
+
+        root.getChildren().add(listView);
+        Scene scene = new Scene(root, 500,500);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Survey Page");
+        primaryStage.show();
     }
 
     public static void main(String[] args) {
         launch();
-
     }
 }
