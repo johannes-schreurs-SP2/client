1bbae73eadd7edc4a38e8e49f5e7de600dae7b85
JohannesSchreurs <johannes@basedesign.com>
2019-09-01 18:36:18 +0200

    Added model for surveys

--- src/model/Survey.java
+++ src/model/Survey.java
@@ -1,6 +1,43 @@
 package model;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
 public class Survey {
 
+    private int id;
+    private String name;
 
+
+    public Survey() {
+    }
+
+    public Survey(int id, String name) {
+        this.id = id;
+        this.name = name;
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return this.name;
+    }
 }
